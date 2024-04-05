ef6a0518d12a9d46bb09dbeffac82df42165757f
EnzoFavret3il <favrete@3il.fr>
2024-04-05 14:00:26 +0200

    [JAVACODE] Classe avec set/get/construct

--- src/Tache/Task.java
+++ src/Tache/Task.java
@@ -1,5 +1,45 @@
 package Tache;
 
-public class Task {
+import java.util.Date;
 
+public class Task {
+	int id;
+	String titre;
+	String description;
+	Date dateEcheance;
+	enum priorite {HAUTE,MOEYENNE,BASSE, AUCUNE}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public Date getDateEcheance() {
+		return dateEcheance;
+	}
+	public void setDateEcheance(Date dateEcheance) {
+		this.dateEcheance = dateEcheance;
+	}
+	public Task(int id, String titre, String description, Date dateEcheance) {
+		super();
+		this.id = id;
+		this.titre = titre;
+		this.description = description;
+		this.dateEcheance = dateEcheance;
+	};
+	
+	
+	
 }

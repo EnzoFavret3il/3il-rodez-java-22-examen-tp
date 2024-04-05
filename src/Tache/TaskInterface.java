8b1d41fe2035cd82a0ac56590de661efcdcf0b87
EnzoFavret3il <favrete@3il.fr>
2024-04-05 14:10:09 +0200

    [JAVADOC] TaskInterface

--- src/Tache/TaskInterface.java
+++ src/Tache/TaskInterface.java
@@ -2,4 +2,8 @@ package Tache;
 
 public interface TaskInterface {
 
+	public void ajouterTache(Task tache);
+	public void modifierTache(Task tache);
+	public void supprimerTache(Task tache);
+	public Task[] getTaches();
 }

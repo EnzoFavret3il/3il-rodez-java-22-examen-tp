f12b1104187b8c499fc8bd4da9b128aaba804a44
EnzoFavret3il <favrete@3il.fr>
2024-04-05 15:22:23 +0200

    Test

---
+++ src/Test/TestMain.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class TestMain {
+
+	public TestMain() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Ceci est un test");
+
+	}
+
+}
